b4f02f29743d09c8eaa840f138cbd297ec243a91
Mateusz Kwiatkowski <mateusz.kwiatkowski@kps.com>
2020-10-29 17:30:58 +0100

    Init repo

---
+++ src/pages/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Under Construction" src="~/assets/img/under-construction.png" width="300" />
+    <h1>Under construction</h1>
+    <p>It takes us some more time, so please excuse us this inconvenience.</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Marketingowy Wir',
+     meta: [
+      {
+        name: 'description',
+        content: 'Blog o online marketingu.'
+      }
+    ],
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
