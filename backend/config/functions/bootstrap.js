adfb28b14ca4d80af43c5012d44a5e71d7aabca4
kwiat1990 <kwiat1990@gmail.com>
2020-12-29 12:42:23 +0100

    sanitize rich-content

--- backend/config/functions/bootstrap.js
+++ backend/config/functions/bootstrap.js
@@ -11,16 +11,4 @@
  */
 
 module.exports = async () => {
-  // TODO: remove before goes to production!
-  // Create some mock posts (make it only one time as sqlite can't handle duplicates)
-  // for (let i = 0; i < 10; i++) {
-  //   strapi.services.category.create({
-  //     name: `Category ${i + 1}`,
-  //   });
-  //   strapi.services.article.create({
-  //     title: `Suspendisse potenti cras in purus ${i + 1}`,
-  //     content:
-  //       "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.\n\nFusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.\n\nSed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.\n\nPellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.\n\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.\n\nCurabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.\n\nIn sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.",
-  //   });
-  // }
 };
--- frontend/src/components/RichContent.vue
+++ frontend/src/components/RichContent.vue
@@ -1,6 +1,7 @@
 <script>
 import marked from "~/utils/markdown-renderer";
-import DOMPurify from "dompurify";
+// import DOMPurify from "dompurify";
+import sanitizeHtml from "sanitize-html";
 
 /**
  *  In order to be able to render a Vue component inside marked() a dynamic component has to be used.
@@ -12,7 +13,8 @@ export default {
   template: `<component :is="transformed"></component>`,
   computed: {
     transformed() {
-      const sanitized = DOMPurify.sanitize(this.content);
+      // const sanitized = DOMPurify.sanitize(this.content);
+      const sanitized = sanitizeHtml(this.content);
       return {
         name: "MarkdownTransformer",
         template: `<div class="prose prose-lg">${marked(sanitized)}</div>`,
